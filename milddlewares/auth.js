069791e8684f7d91d63128e9a03003cf47326929
NataponJ <nataponj@gmail.com>
2024-02-18 12:36:05 +0700

    roll back auth and createJwt

--- milddlewares/auth.js
+++ milddlewares/auth.js
@@ -1,23 +1,21 @@
 import jwt from "jsonwebtoken";
-import { ObjectId } from 'mongodb';
 
 export const auth = (req, res, next) => {
   const authHeader = req.headers.authorization;
   const jwtSecretKey = process.env.JWT_SECRET_KEY;
 
   if (!authHeader || !authHeader.startsWith("Bearer")) {
-    return res.status(401).send({ error: { message: "Unauthorized" } });
+    return res.status(401).send({ error: { messesage: "Unauthorized" } });
   }
+
   const token = authHeader.split(" ")[1];
+
   try {
     const decoded = jwt.verify(token, jwtSecretKey);
-    if (!ObjectId.isValid(decoded.userId)) {
-      return res.status(400).send({ error: { message: "Invalid userId in token" } });
-    }
     req.user = decoded;
     next();
   } catch (error) {
     console.log(error);
-    res.status(400).send({ error: { message: "Invalid token" } });
+    res.status(400).send({ error: { messesage: "Invalid token" } });
   }
 };
--- milddlewares/createJwt.js
+++ milddlewares/createJwt.js
@@ -1,9 +1,9 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
-export const createJwt = ({ userId, email }) => {
+export const createJwt = ( { userId, email}) => {
   const jwtSecretKey = process.env.JWT_SECRET_KEY;
-  const token = jwt.sign({ userId: userId.toString(), email: email }, jwtSecretKey, {
+  const token = jwt.sign({ userId: userId, email: email }, jwtSecretKey, {
     expiresIn: "8h",
   });
   return token;
-};
+}
